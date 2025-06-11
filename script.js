73ff0e466f85f925c7da255524900ee2cbcfaa36
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-06-11 06:29:11 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const form = document.getElementById('registrationForm');
 
 const feedbackDiv = document.getElementById('form-feedback');
+const feedbackDiv = document.getElementById('form-feedback');
