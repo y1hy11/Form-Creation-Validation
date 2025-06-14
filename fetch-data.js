688fe4f584373265015f56a655d8ef6b385f3302
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-06-14 19:49:25 +0100

    update

--- fetch-data.js
+++ fetch-data.js
@@ -1,26 +1,26 @@
 async function fetchUserData() {
-    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
-    const dataContainer = document.getElementById('api-data');
-    try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const users = await response.json();
-        
-        dataContainer.innerHTML = '';
-        
-        const userList = document.createElement('ul');
-        users.forEach(user => {
-            const listItem = document.createElement('li');
-            listItem.textContent = user.name;
-            userList.appendChild(listItem);
-        });
-        
-        dataContainer.appendChild(userList);
-    } catch (error) {
-        dataContainer.textContent = 'Failed to load user data.';
-        console.error('There was a problem with the fetch operation:', error);
+  const apiUrl = "https://jsonplaceholder.typicode.com/users";
+  const dataContainer = document.getElementById("api-data");
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+    const users = await response.json();
+
+    dataContainer.innerHTML = "";
+
+    const userList = document.createElement("ul");
+    users.forEach((user) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = user.name;
+      userList.appendChild(listItem);
+    });
+
+    dataContainer.appendChild(userList);
+  } catch (error) {
+    dataContainer.textContent = "Failed to load user data.";
+    console.error("There was a problem with the fetch operation:", error);
+  }
 }
-document.addEventListener('DOMContentLoaded', fetchUserData);
+document.addEventListener("DOMContentLoaded", fetchUserData);
